b69b53a534593f8a84fed8ba416d50c41ff2cdd6
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-03-09 20:27:30 +0100

    Updated English translation

--- src/qr-encoder_en_GB.ts
+++ src/qr-encoder_en_GB.ts
@@ -1,68 +1,91 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en" sourcelanguage="en">
+<TS version="2.1" language="en_US" sourcelanguage="en_US">
 <context>
-	<name>MainWindow</name>
-	<message>
-		<location filename="mainwindow.ui" line="14"/>
-		<source>QR Encoder</source>
-		<translation>QR Encoder</translation>
-	</message>
-	<message>
-		<location filename="mainwindow.ui" line="75"/>
-		<source>Text</source>
-		<translation>Text</translation>
-	</message>
-	<message>
-		<location filename="mainwindow.ui" line="105"/>
-		<source>Generate QR</source>
-		<translation>Generate QR</translation>
-	</message>
-	<message>
-		<location filename="mainwindow.ui" line="112"/>
-		<source>Save QR Code</source>
-		<translation>Save QR Code</translation>
-	</message>
-	<message>
-		<location filename="mainwindow.ui" line="123"/>
-		<source>Shrinking factor</source>
-		<translation>Shrinking factor</translation>
-	</message>
-	<message>
-		<location filename="mainwindow.ui" line="177"/>
-		<source>Advanced options</source>
-		<translation>Advanced options</translation>
-	</message>
-	<message>
-		<location filename="mainwindow.ui" line="185"/>
-		<source>ECC                  </source>
-		<translation>ECC</translation>
-	</message>
-	<message>
-		<location filename="mainwindow.ui" line="192"/>
-		<location filename="mainwindow.ui" line="211"/>
-		<source>High</source>
-		<translation>High</translation>
-	</message>
-	<message>
-		<location filename="mainwindow.ui" line="196"/>
-		<source>Low</source>
-		<translation>Low</translation>
-	</message>
-	<message>
-		<location filename="mainwindow.ui" line="201"/>
-		<source>Medium</source>
-		<translation>Medium</translation>
-	</message>
-	<message>
-		<location filename="mainwindow.ui" line="206"/>
-		<source>Quartile</source>
-		<translation>Quartile</translation>
-	</message>
-	<message>
-		<location filename="mainwindow.ui" line="236"/>
-		<source>Compression</source>
-		<translation>Compression</translation>
-	</message>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>QR Encoder</source>
+        <translation>QR Encoder</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="75"/>
+        <source>Text</source>
+        <translation>Text</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="105"/>
+        <source>Generate QR</source>
+        <translation>Generate QR</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="112"/>
+        <source>Save QR Code</source>
+        <translation>Save QR Code</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="123"/>
+        <source>Shrinking factor</source>
+        <translation>Shrinking factor</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="177"/>
+        <source>Advanced options</source>
+        <translation>Advanced options</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="185"/>
+        <source>ECC                  </source>
+        <translation>ECC</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="192"/>
+        <location filename="mainwindow.ui" line="211"/>
+        <source>High</source>
+        <translation>High</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="196"/>
+        <source>Low</source>
+        <translation>Low</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="201"/>
+        <source>Medium</source>
+        <translation>Medium</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="206"/>
+        <source>Quartile</source>
+        <translation>Quartile</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="236"/>
+        <source>Compression</source>
+        <translation>Compression</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="mainwindow.cpp" line="49"/>
+        <source>Low</source>
+        <translation>Low</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="50"/>
+        <source>Medium</source>
+        <translation>Medium</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="51"/>
+        <source>Quartile</source>
+        <translation>Quartile</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="52"/>
+        <source>High</source>
+        <translation>High</translation>
+    </message>
 </context>
 </TS>
