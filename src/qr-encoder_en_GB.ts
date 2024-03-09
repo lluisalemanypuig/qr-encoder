ec414a7914eb1ea2f0d401164266697662cb1cc8
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-03-09 20:07:31 +0100

    Updated English translation

--- src/qr-encoder_en_GB.ts
+++ src/qr-encoder_en_GB.ts
@@ -1,3 +1,68 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_GB"></TS>
+<TS version="2.1" language="en" sourcelanguage="en">
+<context>
+	<name>MainWindow</name>
+	<message>
+		<location filename="mainwindow.ui" line="14"/>
+		<source>QR Encoder</source>
+		<translation>QR Encoder</translation>
+	</message>
+	<message>
+		<location filename="mainwindow.ui" line="75"/>
+		<source>Text</source>
+		<translation>Text</translation>
+	</message>
+	<message>
+		<location filename="mainwindow.ui" line="105"/>
+		<source>Generate QR</source>
+		<translation>Generate QR</translation>
+	</message>
+	<message>
+		<location filename="mainwindow.ui" line="112"/>
+		<source>Save QR Code</source>
+		<translation>Save QR Code</translation>
+	</message>
+	<message>
+		<location filename="mainwindow.ui" line="123"/>
+		<source>Shrinking factor</source>
+		<translation>Shrinking factor</translation>
+	</message>
+	<message>
+		<location filename="mainwindow.ui" line="177"/>
+		<source>Advanced options</source>
+		<translation>Advanced options</translation>
+	</message>
+	<message>
+		<location filename="mainwindow.ui" line="185"/>
+		<source>ECC                  </source>
+		<translation>ECC</translation>
+	</message>
+	<message>
+		<location filename="mainwindow.ui" line="192"/>
+		<location filename="mainwindow.ui" line="211"/>
+		<source>High</source>
+		<translation>High</translation>
+	</message>
+	<message>
+		<location filename="mainwindow.ui" line="196"/>
+		<source>Low</source>
+		<translation>Low</translation>
+	</message>
+	<message>
+		<location filename="mainwindow.ui" line="201"/>
+		<source>Medium</source>
+		<translation>Medium</translation>
+	</message>
+	<message>
+		<location filename="mainwindow.ui" line="206"/>
+		<source>Quartile</source>
+		<translation>Quartile</translation>
+	</message>
+	<message>
+		<location filename="mainwindow.ui" line="236"/>
+		<source>Compression</source>
+		<translation>Compression</translation>
+	</message>
+</context>
+</TS>
