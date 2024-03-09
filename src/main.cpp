aaa933137d18a4b979f1e76cfa88f7d1bdfb7fc1
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-03-09 20:26:33 +0100

    Minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -31,8 +31,8 @@ int main(int argc, char *argv[]) {
 
 	QTranslator translator;
 	const QStringList uiLanguages = QLocale::system().uiLanguages();
-	for (const QString &locale : uiLanguages) {
-        const QString baseName = "qr-encoder_" + QLocale(locale).name();
+	for (const QString& locale : uiLanguages) {
+		const QString baseName = "qr-encoder_" + QLocale(locale).name();
 		if (translator.load(":/i18n/" + baseName)) {
 			a.installTranslator(&translator);
 			break;
